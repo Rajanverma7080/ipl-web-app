418c55d5009ec514c248327660d4d786a3432c66
Rajanverma7080 <rajanverma2242@gmail.com>
2026-08-29 12:41:03 +0530

    create dashboard UI

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app=Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return 'create and connect to the server'
+    return render_template('index.html')
 
-
-app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True ,port=7000)
