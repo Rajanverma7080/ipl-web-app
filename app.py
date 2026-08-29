8b28edf524f123f3a6eb9d571fa4d177984cd6e8
Rajanverma7080 <rajanverma2242@gmail.com>
2026-08-29 15:54:06 +0530

    add view ipl all teams features

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 app=Flask(__name__)
 
@@ -6,6 +6,68 @@ app=Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+
+# @app.route('/teams')
+@app.route("/teams")
+def teams():
+
+    teams = [
+        {
+            "name": "Chennai Super Kings",
+            "short_name": "CSK",
+            "logo": "CSK_logo.png"
+        },
+        {
+            "name": "Mumbai Indians",
+            "short_name": "MI",
+            "logo": "MI_logo.png"
+        },
+        {
+            "name": "Royal Challengers Bangalore",
+            "short_name": "RCB",
+            "logo": "RCB_logo.png"
+        },
+        {
+            "name": "Kolkata Knight Riders",
+            "short_name": "KKR",
+            "logo": "KKR_logo.png"
+        },
+        {
+            "name": "Rajasthan Royals",
+            "short_name": "RR",
+            "logo": "RR_logo.png"
+        },
+        {
+            "name": "Rajasthan Royals",
+            "short_name": "RR",
+            "logo": "DC_logo.png"
+        },
+        {
+            "name": "Rajasthan Royals",
+            "short_name": "RR",
+            "logo": "LSG_logo.png"
+        },
+        {
+            "name": "Rajasthan Royals",
+            "short_name": "RR",
+            "logo": "PBKS_logo.png"
+        },
+        {
+            "name": "Rajasthan Royals",
+            "short_name": "RR",
+            "logo": "SRH_logo.png"
+        },
+        {
+            "name": "Rajasthan Royals",
+            "short_name": "RR",
+            "logo": "GT_logo.png"
+        },
+    ]
+
+    return render_template(
+        "teams.html",
+        teams=teams
+    )
 
 if __name__ == '__main__':
     app.run(debug=True ,port=7000)
